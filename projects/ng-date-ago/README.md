a396daa36a32d0d254ea574dbe8f8c1b51489156
shyambandla <shyambandla143@gmail.com>
2021-11-05 18:44:29 +0530

    added left support

--- projects/ng-date-ago/README.md
+++ projects/ng-date-ago/README.md
@@ -1,24 +1,42 @@
-# NgDateAgo
+# Installation
 
-This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.0.
+install using npm
 
-## Code scaffolding
+```
+npm i ng-date-ago
+```
 
-Run `ng generate component component-name --project ngDateAgo` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module --project ngDateAgo`.
-> Note: Don't forget to add `--project ngDateAgo` or else it will be added to the default project in your `angular.json` file. 
+or
 
-## Build
+add directly using ng add
 
-Run `ng build ngDateAgo` to build the project. The build artifacts will be stored in the `dist/` directory.
+```
+ng add ng-date-ago
+```
 
-## Publishing
+# Usage
 
-After building your library with `ng build ngDateAgo`, go to the dist folder `cd dist/ng-date-ago` and run `npm publish`.
+import to app module
 
-## Running unit tests
+```typescript
+import { NgDateAgoModule } from 'ng-date-ago/';
+@NgModule({
+  declarations: [
+   ...
+  ],
+  imports: [
+    ...,
+    NgDateAgoModule
+  ],
+  providers: [
+    ],
+  bootstrap: [...]
+})
+export class AppModule { }
+```
 
-Run `ng test ngDateAgo` to execute the unit tests via [Karma](https://karma-runner.github.io).
+in html
 
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+```html
+<p>{{isoDateString | dateAgo}}</p>
+```
--- projects/ng-date-ago/src/lib/date-ago.pipe.ts
+++ projects/ng-date-ago/src/lib/date-ago.pipe.ts
@@ -8,7 +8,9 @@ export class DateAgoPipe implements PipeTransform {
 
     transform(value: any, args?: any): any {
         if (value) {
-            const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+            let seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+           console.log(seconds)
+            if(seconds>=0){
             if (seconds < 29) // less than 30 seconds ago will show as 'Just now'
                 return 'Just now';
             const intervalsKey = [
@@ -39,8 +41,44 @@ export class DateAgoPipe implements PipeTransform {
                         return counter + ' ' + intervalsKey[i] + 's ago'; // plural (2 days ago)
                     }
             }
+        }else if(seconds<0){
+// Then
+            seconds=seconds*-1;
+            
+            const intervalsKey = [
+                "year",
+                "month",
+                "week",
+                "day",
+                "hour",
+                "minute",
+                "second"
+            ];
+            const intervalsValue = [
+               31536000,
+            2592000,
+           604800,
+               86400,
+               3600,
+               60,
+              1
+            ];
+            let counter;
+            for (let i=0;i<intervalsKey.length;i++) {
+                counter = Math.floor(seconds / intervalsValue[i]);
+                
+                if (counter > 0)
+                    if (counter === 1) {
+                        return counter + ' ' + intervalsKey[i] + ' left'; // singular (1 day ago)
+                    } else {
+                        return counter + ' ' + intervalsKey[i] + 's left'; // plural (2 days ago)
+                    }
+            }
         }
-        return value;
     }
 
+   
+        return value;
+    
+    }
 }
